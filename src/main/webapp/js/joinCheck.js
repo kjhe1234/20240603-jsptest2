72833a992cb4ee9292edcc17f90374834ecb9bce
kjhe1234 <kjhe1234@naver.com>
2024-06-03 12:15:45 +0900

    js 및 유효성 검사 연습

---
+++ src/main/webapp/js/joinCheck.js
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+
+ function joinCheck(){
+	
+	var form = document.joinForm;
+	/* 아이디가 공란으로 입력되었는지 유효성 검사 */
+	if(form.memberId.value.length == 0) {
+		alert("아이디는 필수 입력사항입니다. \n다시 확인해 주세요.");
+		form.memberId.focus();
+		return false;
+	}
+	/* 아이디는 4자 이상 10자 이하만 가능하도록 유효성 검사*/
+	if(form.memberId.value.length < 4 || document.joinForm.memberId.value.length > 15) {
+		alert("아이디는 4자 이상 15자 이하로만 만드실수 있습니다. \n다시 확인해 주세요.");
+		form.memberId.focus();
+		return false;
+	}
+	
+	
+	if(form.memberPw.value.length == 0) {
+		alert("비밀번호는 필수 입력사항입니다. \n다시 확인해 주세요.");
+		form.memberPw.focus();
+		return false;
+	}
+	/* 비밀번호는 5자 이상만 가능하도록 유효성 검사*/
+	if(form.memberPw.value.length < 5 ) {
+		alert("비밀번호는 5자 이상만 입력 가능합니다. \n다시 확인해 주세요.");
+		form.memberPw.focus();
+		return false;
+	}
+
+	if(document.joinForm.memberName.value.length == 0) {
+		alert("이름은 필수 입력사항입니다. \n다시 확인해 주세요.");
+		return false;
+	}
+	
+	/* 이름은 한글만 입력 가능하도록 유효성 검사*/
+	var hangul = /^[ㄱㅏ-힣]*$/;
+	/* var english = /^[a-z|A-Z]*$/; */
+	
+	if(!hangul.test(document.joinForm.memberName.value)) {
+		alert("이름은 한글만 가능 합니다. \n다시 확인해 주세요.");
+		return false;				
+	}
+	
+	
+	if(document.joinForm.phoneNumber.value.length == 0) {
+		alert("전화번호는 필수 입력사항입니다. \n다시 확인해 주세요.");
+		return false;
+	}
+	
+	/* 전화번호는 숫자만만 입력 가능하도록 유효성 검사*/
+	var number = /^[0-9]*$/;
+	if(!number.test(document.joinForm.phoneNumber.value)) {
+		alert("전화번호는 숫자만 가능 합니다. \n다시 확인해 주세요.");
+		return false;				
+	}
+	
+	
+	
+	return true;
+ }
---
+++ src/main/webapp/js/test.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+
+ function loginCheck() {
+	alert("자바스크립트 실행!!");	
+ }
+ 
+ 
+ 
